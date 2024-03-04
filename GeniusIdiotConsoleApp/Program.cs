6bdc970a8fd34c4f5f4b751acd2780ed2eebb268
Ivan Povaliaev <povaliaev.ii@yandex.ru>
2024-03-04 20:26:28 +0300

    Выполнил доп. задание №1: - сделал метод универсальным - создано правило определения диагноза

--- GeniusIdiotConsoleApp/Program.cs
+++ GeniusIdiotConsoleApp/Program.cs
@@ -2,11 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 
+/*
+    Добавить возможность вычисления диагноза для любого количества вопросов.
+    Сделать метод универсальным, то есть при добавлении нового вопроса, метод вычисления диагноза не меняется.
+    Количество диагнозов также не меняется.
+    Разработать правило, по которому ставятся диагнозы.
+*/
+
 namespace GeniusIdiotConsoleApp
 {
     internal class Program
     {
-        static List<Question> GetQuestions(int countQuestions)
+        static List<Question> GetQuestions()
         {
             var questions = new List<Question>();
             questions.Add(new Question("Сколько будет два плюс два умноженное на два?", 6));
@@ -14,35 +21,47 @@ namespace GeniusIdiotConsoleApp
             questions.Add(new Question("На двух руках 10 пальцев. Сколько пальцев на 5 руках?", 25));
             questions.Add(new Question("Укол делают каждые полчаса. Сколько нужно минут, чтобы сделать три укола?", 60));
             questions.Add(new Question("Пять свечей горело, две потухли. Сколько свечей осталось?", 2));
+            questions.Add(new Question("Пять свечей горело, две потухли. Сколько свечей осталось?", 2));
+            questions.Add(new Question("Пять свечей горело, две потухли. Сколько свечей осталось?", 2));
+            questions.Add(new Question("Пять свечей горело, две потухли. Сколько свечей осталось?", 2));
+            questions.Add(new Question("Укол делают каждые полчаса. Сколько нужно минут, чтобы сделать три укола?", 60));
+            questions.Add(new Question("Укол делают каждые полчаса. Сколько нужно минут, чтобы сделать три укола?", 60));
+            questions.Add(new Question("На двух руках 10 пальцев. Сколько пальцев на 5 руках?", 25));
+
             return questions;
         }
-        static string[] GetDiagnoses(int countDiagnoses)
+        static string GetDiagnosis(int countRigthAnswers, int countQuestions) //Доп. задача 1 - изменена функция
         {
-            var diagnoses = new string[countDiagnoses];
+            var diagnoses = new string[6];
             diagnoses[0] = "Идиот";
             diagnoses[1] = "Кретин";
             diagnoses[2] = "Дурак";
             diagnoses[3] = "Нормальный";
             diagnoses[4] = "Талант";
             diagnoses[5] = "Гений";
-            return diagnoses;
+            //Доп. задача №1: правило постановки диагноза
+            int percentRightAnswers = 100 * countRigthAnswers / countQuestions;
+            if (percentRightAnswers < 10) return diagnoses[0];
+            if (percentRightAnswers < 30) return diagnoses[1];
+            if (percentRightAnswers < 50) return diagnoses[2];
+            if (percentRightAnswers < 70) return diagnoses[3];
+            if (percentRightAnswers < 90) return diagnoses[4];
+            return diagnoses[5];
         }
 
         static void Main()
         {
             Console.WriteLine("Введите имя пользователя:");
             string username = Console.ReadLine();
-
-            int countQuestions = 5; //количество вопросов
-            Random random = new Random();
             bool testRepeat = true; //переменная для повтора теста
             
             //цикл прохождения теста
             while (testRepeat)
             {
-                var questions = GetQuestions(countQuestions).Shuffle().ToList(); //Сразу перемешиваем список вопросов            
+                var questions = GetQuestions().Shuffle().ToList(); //Сразу перемешиваем список вопросов            
 
                 int countRigthAnswers = 0;
+                int countQuestions = questions.Count(); //Для доп. задачи определяем количество вопросов
 
                 for (int i = 0; i < countQuestions; i++)
                 {
@@ -52,11 +71,11 @@ namespace GeniusIdiotConsoleApp
                     if (userAnswer == questions[i].Answer) countRigthAnswers++;
                 }
 
-                var diagnoses = GetDiagnoses(countQuestions + 1);
+                var diagnosis = GetDiagnosis(countRigthAnswers, countQuestions);
 
                 Console.WriteLine($"Пользователь {username}");
                 Console.WriteLine($"Количество правильных ответов: {countRigthAnswers}");
-                Console.WriteLine($"Ваш диагноз: {diagnoses[countRigthAnswers]}");
+                Console.WriteLine($"Ваш диагноз: {diagnosis}");
                 Console.WriteLine("\nТест завершён. Хотите пройти тест заново?(Введите да/нет)");
                 
                 //цикл вопроса о повторении
